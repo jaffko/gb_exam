2938f7f28fb0fa6e3033ce625b09b9a200f046d4
Ildar <steel14@yandex.ru>
2022-10-31 12:46:50 +0500

    added code, changed README.md

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Find3DigitsStrings(string[] str)
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3) count++;
+    }
+    string[] result = new string[count];
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3) 
+        {
+            result[j] = str[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите размерность массива");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] str = new string[n];
+System.Console.WriteLine("Введите строки массива");
+for (int i = 0; i < n; i++)
+{
+    str[i] = Console.ReadLine();
+}
+System.Console.WriteLine("["+string.Join(", ", Find3DigitsStrings(str))+"]");
